db8361afb599db8ffe4d4266bb6139c5de70dee5
Skiba.Evgeniy <Skiba.Evgeniy@gmail.com>
2023-02-20 23:43:28 +0200

    ProductRepository interface

---
+++ ShopMeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.product;
+
+import com.shopme.admin.user.common.entity.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+}
